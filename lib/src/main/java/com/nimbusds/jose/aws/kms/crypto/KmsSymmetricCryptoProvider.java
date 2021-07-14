bd5a4806ec3b7f130668ab62a85d9508bd64278e
Jitendra Pawar <jitpawa@amazon.com>
2021-07-14 14:24:43 +0530

    Added Encrpter Code

--- lib/src/main/java/com/nimbusds/jose/aws/kms/crypto/KmsSymmetricCryptoProvider.java
+++ lib/src/main/java/com/nimbusds/jose/aws/kms/crypto/KmsSymmetricCryptoProvider.java
@@ -2,7 +2,6 @@ package com.nimbusds.jose.aws.kms.crypto;
 
 import com.amazonaws.services.kms.model.EncryptionAlgorithmSpec;
 import com.nimbusds.jose.JWEAlgorithm;
-import com.nimbusds.jose.KeyLengthException;
 import com.nimbusds.jose.crypto.impl.ContentCryptoProvider;
 import com.nimbusds.jose.crypto.impl.PublicBaseJWEProvider;
 import java.util.Collections;
@@ -12,7 +11,7 @@ import java.util.Set;
 public abstract class KmsSymmetricCryptoProvider extends PublicBaseJWEProvider {
 
     /**
-     * The supported JWE algorithms by the AES crypto provider class.
+     * The supported JWE algorithms by the AWS crypto provider class.
      */
     public static final Set<JWEAlgorithm> SUPPORTED_ALGORITHMS;
 
@@ -22,34 +21,10 @@ public abstract class KmsSymmetricCryptoProvider extends PublicBaseJWEProvider {
         SUPPORTED_ALGORITHMS = Collections.unmodifiableSet(algs);
     }
 
-
-    /**
-     * The Key Encryption Key (KEK).
-     */
-    private final String kek;
-
-
     /**
      * Creates a new AES encryption / decryption provider.
-     *
-     * @param kek The Key Encryption Key. Must be 128 bits (16 bytes), 192 bits (24 bytes) or 256 bits (32 bytes). Must
-     *            not be {@code null}.
-     * @throws KeyLengthException If the KEK length is invalid.
      */
-    protected KmsSymmetricCryptoProvider(final String kek) {
-
+    protected KmsSymmetricCryptoProvider() {
         super(SUPPORTED_ALGORITHMS, ContentCryptoProvider.SUPPORTED_ENCRYPTION_METHODS);
-
-        this.kek = kek;
     }
-
-    /**
-     * Gets the Key Encryption Key (KEK).
-     *
-     * @return The Key Encryption Key.
-     */
-    public String getKey() {
-        return kek;
-    }
-
 }
--- lib/src/main/java/com/nimbusds/jose/aws/kms/crypto/KmsSymmetricEncrypter.java
+++ lib/src/main/java/com/nimbusds/jose/aws/kms/crypto/KmsSymmetricEncrypter.java
@@ -1,11 +1,29 @@
 package com.nimbusds.jose.aws.kms.crypto;
 
 import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.model.DependencyTimeoutException;
+import com.amazonaws.services.kms.model.DisabledException;
+import com.amazonaws.services.kms.model.GenerateDataKeyRequest;
+import com.amazonaws.services.kms.model.GenerateDataKeyResult;
+import com.amazonaws.services.kms.model.InvalidGrantTokenException;
+import com.amazonaws.services.kms.model.InvalidKeyUsageException;
+import com.amazonaws.services.kms.model.KMSInternalException;
+import com.amazonaws.services.kms.model.KMSInvalidStateException;
+import com.amazonaws.services.kms.model.KeyUnavailableException;
+import com.amazonaws.services.kms.model.NotFoundException;
+import com.nimbusds.jose.EncryptionMethod;
 import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEAlgorithm;
 import com.nimbusds.jose.JWECryptoParts;
 import com.nimbusds.jose.JWEEncrypter;
 import com.nimbusds.jose.JWEHeader;
+import com.nimbusds.jose.KeyException;
 import com.nimbusds.jose.KeyLengthException;
+import com.nimbusds.jose.crypto.impl.AlgorithmSupportMessage;
+import com.nimbusds.jose.crypto.impl.ContentCryptoProvider;
+import com.nimbusds.jose.util.Base64URL;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import lombok.NonNull;
 
 public class KmsSymmetricEncrypter extends KmsSymmetricCryptoProvider implements JWEEncrypter {
@@ -13,67 +31,65 @@ public class KmsSymmetricEncrypter extends KmsSymmetricCryptoProvider implements
     @NonNull
     private final AWSKMS kms;
 
+    @NonNull
+    private final String keyId;
+
+
     /**
      * Creates a new AES encrypter.
      *
-     * @param kek The Key Encryption Key. Must be 128 bits (16 bytes), 192 bits (24 bytes) or 256 bits (32 bytes). Must
-     *            not be {@code null}.
+     * @param keyId The Key Encryption Key. Must be 128 bits (16 bytes), 192 bits (24 bytes) or 256 bits (32 bytes).
+     *              Must not be {@code null}.
      * @param kms
      * @throws KeyLengthException If the KEK length is invalid.
      */
-    public KmsSymmetricEncrypter(final String kek, final AWSKMS kms)
+    public KmsSymmetricEncrypter(final String keyId, final AWSKMS kms)
             throws KeyLengthException {
-        super(kek);
         this.kms = kms;
+        this.keyId = keyId;
     }
 
     @Override
     public JWECryptoParts encrypt(final JWEHeader header, final byte[] clearText)
             throws JOSEException {
 
-//        final JWEAlgorithm alg = header.getAlgorithm();
-//
-//
-//        if (!alg.equals(JWEAlgorithm.parse(EncryptionAlgorithmSpec.SYMMETRIC_DEFAULT.toString()))) {
-//            throw new JOSEException(AlgorithmSupportMessage.unsupportedJWEAlgorithm(alg, SUPPORTED_ALGORITHMS));
-//        }
-//
-//        final JWEHeader updatedHeader; // We need to work on the header
-//        final Base64URL encryptedKey; // The second JWE part
-//
-//        // Generate and encrypt the CEK according to the enc method
-//        final EncryptionMethod enc = header.getEncryptionMethod();
-//        // data gene
-//
-//        final SecretKey cek = ContentCryptoProvider.generateCEK(enc, getJCAContext().getSecureRandom());
+        final JWEAlgorithm alg = header.getAlgorithm();
 
-//        if (KmsSymmetricEncrypter.AlgFamily.AESKW.equals(algFamily)) {
-//
-//            encryptedKey = Base64URL.encode(AESKW.wrapCEK(cek, getKey(), getJCAContext().getKeyEncryptionProvider()));
-//            updatedHeader = header; // simply copy ref
-//
-//        } else {
-//            // This should never happen
-//            throw new JOSEException("Unexpected JWE algorithm: " + alg);
-//        }
-        return null;
-    }
-//        return ContentCryptoProvider.encrypt(updatedHeader, clearText, cek, encryptedKey, getJCAContext());
-//    }
-//
-//    private GenerateDataKeyResult generateDataKey(){
-//        try {
-//            return kms.generateDataKey(buildGenerateDataKeyRequest());
-//        } catch (NotFoundException | DisabledException | InvalidKeyUsageException e) {
-//            throw new PartnerGatewayConfigurationException(
-//                    String.format(
-//                            "An error was thrown from KMS due to invalid config. Config: %s",
-//                            config),
-//                    e);
-//        } catch (KeyUnavailableException | DependencyTimeoutException | InvalidGrantTokenException
-//                | KMSInternalException | KMSInvalidStateException e) {
-//            throw new PartnerGatewayDependencyException("A temporary error was thrown from KMS.", e);
-//        }
-//    }
+        if (!SUPPORTED_ALGORITHMS.contains(alg)) {
+            throw new JOSEException(AlgorithmSupportMessage.unsupportedJWEAlgorithm(alg, SUPPORTED_ALGORITHMS));
+        }
 
+        final JWEHeader updatedHeader; // We need to work on the header
+        final Base64URL encryptedKey; // The second JWE part
+
+        // Generate and encrypt the CEK according to the enc method
+        final EncryptionMethod enc = header.getEncryptionMethod();
+        // data gene
+        GenerateDataKeyResult generateDataKeyResult = generateDataKey(keyId, header.getAlgorithm().toString());
+        final SecretKey cek = new SecretKeySpec(generateDataKeyResult.getPlaintext().array(), "AES");
+
+        encryptedKey = Base64URL.encode(generateDataKeyResult.getCiphertextBlob().array());
+        updatedHeader = header; // simply copy ref
+
+        return ContentCryptoProvider.encrypt(updatedHeader, clearText, cek, encryptedKey, getJCAContext());
     }
+
+    private GenerateDataKeyResult generateDataKey(String keyId, String encryptionMethod) throws JOSEException {
+        try {
+            return kms.generateDataKey(buildGenerateDataKeyRequest(keyId, encryptionMethod));
+        } catch (NotFoundException | DisabledException | InvalidKeyUsageException | KeyUnavailableException
+                | KMSInvalidStateException e) {
+            throw new KeyException("An error occurred while using Key");
+        } catch (DependencyTimeoutException | InvalidGrantTokenException
+                | KMSInternalException e) {
+            throw new JOSEException("A temporary error was thrown from KMS.");
+        }
+    }
+
+    private GenerateDataKeyRequest buildGenerateDataKeyRequest(String keyId, String encryptionMethod) {
+        GenerateDataKeyRequest generateDataKeyRequest = new GenerateDataKeyRequest();
+        generateDataKeyRequest.setKeyId(keyId);
+        generateDataKeyRequest.setKeySpec(encryptionMethod);
+        return generateDataKeyRequest;
+    }
+}
