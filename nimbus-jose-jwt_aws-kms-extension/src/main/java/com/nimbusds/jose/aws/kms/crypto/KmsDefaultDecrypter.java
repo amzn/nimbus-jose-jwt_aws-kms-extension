5e6995220a053b2c529d53eb1206481db6040107
Yash Agrawal <yasagra@amazon.com>
2024-02-06 18:10:06 +0530

    Added KmsDefaultDecrypter

---
+++ nimbus-jose-jwt_aws-kms-extension/src/main/java/com/nimbusds/jose/aws/kms/crypto/KmsDefaultDecrypter.java
@@ -0,0 +1,127 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package com.nimbusds.jose.aws.kms.crypto;
+
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.model.DecryptRequest;
+import com.amazonaws.services.kms.model.DecryptResult;
+import com.amazonaws.services.kms.model.DependencyTimeoutException;
+import com.amazonaws.services.kms.model.DisabledException;
+import com.amazonaws.services.kms.model.InvalidGrantTokenException;
+import com.amazonaws.services.kms.model.InvalidKeyUsageException;
+import com.amazonaws.services.kms.model.KMSInternalException;
+import com.amazonaws.services.kms.model.KMSInvalidStateException;
+import com.amazonaws.services.kms.model.KeyUnavailableException;
+import com.amazonaws.services.kms.model.NotFoundException;
+import com.nimbusds.jose.CriticalHeaderParamsAware;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEAlgorithm;
+import com.nimbusds.jose.JWEDecrypter;
+import com.nimbusds.jose.JWEHeader;
+import com.nimbusds.jose.RemoteKeySourceException;
+import com.nimbusds.jose.aws.kms.crypto.impl.KmsDefaultEncryptionCryptoProvider;
+import com.nimbusds.jose.aws.kms.exceptions.TemporaryJOSEException;
+import com.nimbusds.jose.crypto.impl.ContentCryptoProvider;
+import com.nimbusds.jose.crypto.impl.CriticalHeaderParamsDeferral;
+import com.nimbusds.jose.util.Base64URL;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.Set;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import lombok.NonNull;
+
+public class KmsDefaultDecrypter extends KmsDefaultEncryptionCryptoProvider implements JWEDecrypter,
+        CriticalHeaderParamsAware {
+
+    /**
+     * The critical header policy.
+     */
+    private final CriticalHeaderParamsDeferral critPolicy = new CriticalHeaderParamsDeferral();
+
+    public KmsDefaultDecrypter(@NonNull final AWSKMS kms,
+                               @NonNull final String keyId,
+                               @NonNull final Map<String, String> encryptionContext) {
+        super(kms, keyId, encryptionContext);
+    }
+
+    public KmsDefaultDecrypter(@NonNull final AWSKMS kms,
+                               @NonNull final String keyId) {
+        super(kms, keyId);
+    }
+
+    public KmsDefaultDecrypter(@NonNull final AWSKMS kms,
+                               @NonNull final String keyId,
+                               @NonNull final Set<String> defCritHeaders) {
+        this(kms, keyId);
+        critPolicy.setDeferredCriticalHeaderParams(defCritHeaders);
+    }
+
+    public KmsDefaultDecrypter(@NonNull final AWSKMS kms,
+                               @NonNull final String keyId,
+                               @NonNull final Map<String, String> encryptionContext,
+                               @NonNull final Set<String> defCritHeaders) {
+        this(kms, keyId, encryptionContext);
+        critPolicy.setDeferredCriticalHeaderParams(defCritHeaders);
+    }
+
+    @Override
+    public Set<String> getProcessedCriticalHeaderParams() {
+        return critPolicy.getProcessedCriticalHeaderParams();
+    }
+
+    @Override
+    public Set<String> getDeferredCriticalHeaderParams() {
+        return critPolicy.getDeferredCriticalHeaderParams();
+    }
+
+    @Override
+    public byte[] decrypt(
+            @NonNull final JWEHeader header,
+            @NonNull final Base64URL encryptedKey,
+            @NonNull final Base64URL iv,
+            @NonNull final Base64URL cipherText,
+            @NonNull final Base64URL authTag)
+            throws JOSEException {
+
+        validateJWEHeader(header);
+        critPolicy.ensureHeaderPasses(header);
+
+        final DecryptResult cekDecryptResult =
+                decryptCek(getKeyId(), getEncryptionContext(), header.getAlgorithm(), encryptedKey);
+        final SecretKey cek =
+                new SecretKeySpec(cekDecryptResult.getPlaintext().array(), header.getAlgorithm().toString());
+        return ContentCryptoProvider.decrypt(header, encryptedKey, iv, cipherText, authTag, cek, getJCAContext());
+    }
+
+    private DecryptResult decryptCek(String keyId, Map<String, String> encryptionContext, JWEAlgorithm alg,
+                                     Base64URL encryptedKey)
+            throws JOSEException {
+        try {
+            return getKms().decrypt(new DecryptRequest()
+                    .withEncryptionContext(encryptionContext)
+                    .withKeyId(keyId)
+                    .withEncryptionAlgorithm(alg.getName())
+                    .withCiphertextBlob(ByteBuffer.wrap(encryptedKey.decode())));
+        } catch (NotFoundException | DisabledException | InvalidKeyUsageException | KeyUnavailableException
+                 | KMSInvalidStateException e) {
+            throw new RemoteKeySourceException("An exception was thrown from KMS due to invalid key.", e);
+        } catch (DependencyTimeoutException | InvalidGrantTokenException | KMSInternalException e) {
+            throw new TemporaryJOSEException("A temporary error was thrown from KMS.", e);
+        }
+    }
+}
